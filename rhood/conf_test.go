2947315d6d8dca34fa537891b7a12f678791155c
zbbucket <zbbucket@gmail.com>
2014-10-30 12:49:18 +0400

    Fix for test.

--- rhood/conf_test.go
+++ rhood/conf_test.go
@@ -9,7 +9,7 @@ func TestLoadConfig(t *testing.T) {
 	configMap := loadTestConfig()
 	result := mapToString(configMap)
 
-	expected := "map[controlBoxListenAddress:192.168.1.1:80 controlBoxPublicAddress:192.168.1.1:8080 dirRoot:/var/www/rhood dirStatic:/var/www/rhood/rhood-www/static dirStoreHtml:/var/www/rhood/rhood-www/static/cache/html/ dirStoreVideo:/var/www/rhood/rhood-www/static/cache/video/ dirTemplates:/var/www/rhood/data/templates/ goappProxyPort:8082 youtubeDownloader:/usr/bin/youtube-dl]"
+	expected := "map[controlBoxListenAddress:192.168.1.1:80 controlBoxPublicAddress:192.168.1.1:8080 controlBoxPublicAddressWebsocket:localhost:2000 dirRoot:/var/www/rhood dirStatic:/var/www/rhood/rhood-www/static dirStoreHtml:/var/www/rhood/rhood-www/static/cache/html/ dirStoreVideo:/var/www/rhood/rhood-www/static/cache/video/ dirTemplates:/var/www/rhood/data/templates/ goappProxyPort:8082 youtubeDownloader:/usr/bin/youtube-dl]"
 
 	assert.Equal(t, expected, result)
 }
